b03ff42ad946db2c8ce992da4e17db359aaa5b18
Fabien Thiombiano <thiombianofabien@gmail.com>
2019-07-24 01:01:31 -0500

    completed data binding

--- ComponentsDataBinding/src/app/app.component.ts
+++ ComponentsDataBinding/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ComponentsDataBinding';
+
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  ngOnInit() {
+
+  }
+
+  onGameStarted(data: number) {
+    console.log(data);
+    if (data % 2 === 0) {
+      this.evenNumbers.push(data);
+    } else {
+      this.oddNumbers.push(data);
+    }
+  }
 }
